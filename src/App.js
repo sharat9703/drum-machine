61ca5a217dcdd14d9d49efc298db772ba9c4a703
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 <sharath.yaragatti@yahoo.com>
2024-05-26 18:46:00 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,92 @@
-import React from 'react';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    document.addEventListener('keydown',(event)=>{
+      playSound(event.key.toUpperCase());
+    });
+  },[]);
+  const [activeKey,setActiveKey] = useState("");
   const drumpad = [
     {
       keyCode: 81,
       text: "Q",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"
     },
     {
       keyCode: 87,
       text: "W",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"
     },
     {
       keyCode: 69,
       text: "E",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"
     },
     {
       keyCode: 65,
       text: "A",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"
     },
     {
       keyCode: 83,
       text: "S",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"
     },
     {
       keyCode: 68,
       text: "D",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"
     },
     {
       keyCode: 90,
       text: "Z",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"
     },
     {
       keyCode: 88,
       text: "X",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"
     },
     {
       keyCode: 67,
       text: "C",
-      src: "https://s3.amazonaws.com/freecodecamp..."
+      src: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"
     }
   ];
+  let playSound =(selector)=>{
+      const audio = document.getElementById(selector);
+      console.log(audio);
+      audio.play();
+      setActiveKey(selector);
+  }
   return (
     <div className="App">
       
+         <div id= "drum-machine">
+         <div id="heading"><h1>My Drum Machine</h1></div>
+            <div id="display">{activeKey}</div>
+                <div className="drum-pads">
+                  {drumpad.map((drumpad)=>(
+                    <div 
+                    onClick={()=>{
+                      playSound(drumpad.text);
+                    }} 
+                    className='drum-pad' 
+                    id={drumpad.src}>
+                      {drumpad.text}
+                      <audio className='clip' id={drumpad.text} src={drumpad.src}> 
+                      </audio>
+                    </div>
+                  ))}
+                </div>
+            
+          </div>       
     </div>
+    
   );
 }
 
 export default App;
+
